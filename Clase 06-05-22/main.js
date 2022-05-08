8c835f80d97c4cc39f6534cc3136a9bc52d400df
unknown <jose.guerra02@inacapmail.cl>
2022-05-08 13:37:53 -0400

    avanazando el formulario

--- Clase 06-05-22/main.js
+++ Clase 06-05-22/main.js
@@ -1,19 +1,10 @@
-let funcion= () =>{
-    //5>4? alert("Hola mundo"):alert('else')
-    let arreglo = []
-  
-
-    for (let i = 0; i < 4; i++){        
-        arreglo[i] = document.getElementById("num" + (i+1) ).value
-    }
-
-    for (let j = 0; j < 4; j++){
-        window.setTimeout(function() {
-            document.getElementById('p'+ j).innerHTML = arreglo[j]
-        }, 2000)
-    }
-
-   
-}
+$(function(){
+        
+    let datePicker = document.getElementById('datePicker');
+    let picker = new Litepicker({
+        element: datePicker,
+        format: 'DD MMMM YYYY'
+    });
+});
 
 
