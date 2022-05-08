505734399a22d852ac8270533bd7de0ebd9c6a66
unknown <jose.guerra02@inacapmail.cl>
2022-05-08 18:33:06 -0400

    arreglando cagazo

--- Clase 06-05-22/main.js
+++ Clase 06-05-22/main.js
@@ -1,32 +1,17 @@
-let verData = () =>{
+let funcion= () =>{
+    //5>4? alert("Hola mundo"):alert('else')
+    let arreglo = []
 
-    infoData = []
 
-    // for (let i = 0; i > 5, i++){
-        
-    //     let dataForm = document.getElementById('form'+[i]).value
-
-    // }
-
-    if (nameForm == ''){
-        alert('No se ingreso ningun nombre')
-    }
-    else{
-        console.log('Nombre Correcto')
+    for (let i = 0; i < 4; i++){        
+        arreglo[i] = document.getElementById("num" + (i+1) ).value
     }
 
-   
-
-
-
-
-
-    
-    
-    
-
-    console.log(nameForm, surForm, dateForm, mailForm, commForm)
-
+    for (let j = 0; j < 4; j++){
+        window.setTimeout(function() {
+            document.getElementById('p'+ j).innerHTML = arreglo[j]
+        }, 2000)
+    }
 }
 
 
