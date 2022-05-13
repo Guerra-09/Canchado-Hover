35bbac84c511f5b8b3fc4425538fdf1e25bfe61b
Jose Guerra <jose.guerra02@inacapmail.cl>
2022-05-13 12:25:07 -0400

    formulario terminado clase 12-05-22

--- Formulario/main.js
+++ Formulario/main.js
@@ -1,47 +1,112 @@
-let verFecha = (fecha) =>{
-
-    let gD = fecha
-
-    let dateGiven = gD.split('-');
-
-    let dateAct = new Date();
-    let aDay = dateAct.getDate();
-    let aMonth = dateAct.getMonth()+1;
-    let aYear = dateAct.getFullYear();
-
-    console.log(dateGiven[0])
-
-  
-
-
-
-}
-
 let verData = () =>{   
-    infoData = []
 
-    for (let i = 0; i < 5; i++){
 
-        infoData.push(document.getElementById('form'+ i).value)
 
-        if (infoData[i] == ''){
-            alert('campos vacios!')
-            break
-        }
+    if (document.getElementById('form_nombre').value == ''){
 
-    }   
-    
-    for (var e = 0; e< infoData[3].length; e++) {
-        var caracter = infoData[3].charAt(e);
-        if( caracter == "@") {
-            console.log("@ esta presente");
-           
-        } else {
-            console.log("@ no esta presente");
-        }
+        document.getElementById('campo_nombre').innerHTML = 'Este campo no puede estar vacio'
+        document.getElementById('campo_nombre').classList.add('invalidText')
+
+        document.getElementById('form_nombre').classList.add('invalidBorder')
+    }else{ 
+        document.getElementById('campo_nombre').innerHTML = 'Nombre aceptado'
+        document.getElementById('campo_nombre').classList.remove('invalidText')
+        document.getElementById('campo_nombre').classList.add('validText')
+
+        document.getElementById('form_nombre').classList.remove('invalidBorder')
+        document.getElementById('form_nombre').classList.add('validBorder')
     }
 
-    verFecha(infoData[2])
+
+
+    if (document.getElementById('form_apellido').value == ''){
+
+        document.getElementById('campo_apellido').innerHTML = 'Este campo no puede estar vacio'
+        document.getElementById('campo_apellido').classList.add('invalidText')
+
+        document.getElementById('form_apellido').classList.add('invalidBorder')
+    }else{
+
+        document.getElementById('campo_apellido').innerHTML = 'Apellido aceptado'
+        document.getElementById('campo_apellido').classList.remove('invalidText')
+        document.getElementById('campo_apellido').classList.add('validText')
+
+        document.getElementById('form_apellido').classList.remove('invalidBorder')
+        document.getElementById('form_apellido').classList.add('validBorder')
+    }
+
+
+
+    if (document.getElementById('form_fecha').value == ''){
+
+        document.getElementById('campo_fecha').innerHTML = 'Este campo no puede estar vacio'
+        document.getElementById('campo_fecha').classList.add('invalidText')
+
+        document.getElementById('form_fecha').classList.add('invalidBorder')
+    }else{
+
+        document.getElementById('campo_fecha').innerHTML = 'Fecha aceptada'
+        document.getElementById('campo_fecha').classList.remove('invalidText')
+        document.getElementById('campo_fecha').classList.add('validText')
+
+        document.getElementById('form_fecha').classList.remove('invalidBorder')
+        document.getElementById('form_fecha').classList.add('validBorder')
+    }
+
+
+    let example = document.getElementById('form_correo').value;
+    let ourSubstring = "@";
+
+
+    if (document.getElementById('form_correo').value == ''){
+
+        document.getElementById('campo_correo').innerHTML = 'Este campo no puede estar vacio'
+        document.getElementById('campo_correo').classList.add('invalidText')
+
+        document.getElementById('form_correo').classList.add('invalidBorder')
+
+    }else if (example.includes(ourSubstring)){
+
+        document.getElementById('campo_correo').innerHTML = 'Correo aceptado'
+        document.getElementById('campo_correo').classList.remove('invalidText')
+        document.getElementById('campo_correo').classList.add('validText')
+
+        document.getElementById('form_correo').classList.remove('invalidBorder')
+        document.getElementById('form_correo').classList.add('validBorder')
+
+    }
+
+    else{
+
+        document.getElementById('campo_correo').innerHTML = 'El correo debe contener @'
+        document.getElementById('campo_correo').classList.add('invalidText')
+
+        document.getElementById('form_correo').classList.add('invalidBorder')
+        
+    }
+
+
+
+
+
+
+
+    if (document.getElementById('form_form').value == ''){
+
+        document.getElementById('campo_form').innerHTML = 'Este campo no puede estar vacio'
+        document.getElementById('campo_form').classList.add('invalidText')
+
+        document.getElementById('form_form').classList.add('invalidBorder')
+    }else{
+
+        document.getElementById('campo_form').innerHTML = 'Formulario aceptado'
+        document.getElementById('campo_form').classList.remove('invalidText')
+        document.getElementById('campo_form').classList.add('validText')
+
+        document.getElementById('form_form').classList.remove('invalidBorder')
+        document.getElementById('form_form').classList.add('validBorder')
+    }
+
 }
 
 
