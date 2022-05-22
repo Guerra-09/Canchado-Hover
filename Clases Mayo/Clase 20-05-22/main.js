7b908430b7c55c9cc76c29c0f84615683f03da4c
Jose Guerra <jose.guerra02@inacapmail.cl>
2022-05-21 20:29:51 -0400

    clase 20

---
+++ Clases Mayo/Clase 20-05-22/main.js
@@ -0,0 +1,52 @@
+let autos = []
+
+
+let formulario = () => {
+
+    let marca = document.getElementById('marcaIngresada').value
+    let modelo = document.getElementById('modeloIngresado').value
+    let year = document.getElementById('anoIngresado').value
+
+    
+
+    let ver = Number.isInteger(year)
+
+    //if corto
+    !isNaN(year) ? console.log('ahora si') : console.log('aun no')
+
+
+
+    let autoNuevo = {
+        marca_obj : marca,
+        modelo_obj : modelo,
+        year_obj : year
+    }
+
+    autos.push(autoNuevo)
+
+    console.log(autos)
+    listar()
+
+    document.getElementById('Form').reset();
+
+}
+
+
+
+
+let listar = () => {
+
+    let listando = document.getElementById('listContainer')
+
+    let html = ""
+
+    autos.forEach(auto => {
+
+        html = html + "<li>"  + auto.marca_obj + " " + auto.modelo_obj + " " + auto.year_obj + "</li>"
+                 
+    });
+
+    listando.innerHTML = html
+}
+
+
