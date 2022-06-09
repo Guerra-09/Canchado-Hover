bbf67878a90df67bea9c2e592bf985c007d7b70e
Jose Guerra <jose.guerra02@inacapmail.cl>
2022-06-09 19:01:16 -0400

    Primer jsx con React

---
+++ Clases Junio/test/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p> Hola Mundo !</p>
+    </div>
+
+    
+  );
+}
+
+export default App;
